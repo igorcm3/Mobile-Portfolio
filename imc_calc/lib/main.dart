0f3106e57cf3c317e25bf93d91f809879fbb657b
Igor Corona de Matos <coronaigor@gmail.com>
2020-07-04 21:24:53 -0300

    Ajustes de layout

--- imc_calc/lib/main.dart
+++ imc_calc/lib/main.dart
@@ -15,12 +15,14 @@ class _HomeState extends State<Home> {
   TextEditingController weigthController = TextEditingController();
   TextEditingController heigthController = TextEditingController();
   GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-  String _infoText = "Informe seus dados";
+  String _infoText = "";
+  String _imcResult = "";
   void _resetFields() {
     weigthController.text = "";
     heigthController.text = "";
     setState(() {
-      _infoText = "Informe seus dados";
+      _imcResult = "";
+      _infoText = "";
       _formkey = GlobalKey<FormState>();
     });
   }
@@ -30,19 +32,19 @@ class _HomeState extends State<Home> {
       double weigth = double.parse(weigthController.text);
       double height = double.parse(heigthController.text) / 100;
       double imc = weigth / (height * height);
-      print(imc);
+      _imcResult = "O IMC calculado é ${imc.toStringAsPrecision(4)}";
       if (imc < 18.6) {
-        _infoText = "Abaixo no peso (${imc.toStringAsPrecision(4)})";
+        _infoText = "Abaixo no peso";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
+        _infoText = "Peso ideal ";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+        _infoText = "Levemente acima do peso";
       } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade grau I (${imc.toStringAsPrecision(4)})";
+        _infoText = "Obesidade grau I";
       } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade grau II (${imc.toStringAsPrecision(4)})";
+        _infoText = "Obesidade grau II";
       } else if (imc >= 40) {
-        _infoText = "Obesidade grau III (${imc.toStringAsPrecision(4)})";
+        _infoText = "Obesidade grau III";
       }
     });
   }
@@ -75,6 +77,14 @@ class _HomeState extends State<Home> {
                   Icons.person_outline,
                   size: 120,
                   color: Colors.lightGreen,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5, bottom: 10),
+                  child: Text(
+                    "Informe seus dados",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  ),
                 ),
                 TextFormField(
                   controller: weigthController,
@@ -113,7 +123,7 @@ class _HomeState extends State<Home> {
                 Padding(
                   padding: EdgeInsets.only(top: 15.0, bottom: 10.0),
                   child: Container(
-                    height: 50.0,
+                    height: 45.0,
                     child: RaisedButton(
                       //TODO: Alterar shape do button
                       onPressed: () {
@@ -126,17 +136,24 @@ class _HomeState extends State<Home> {
                         side: BorderSide(color: Colors.green),
                       ),
                       child: Text(
-                        "Calcular",
-                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        "Calcular".toUpperCase(),
+                        style: TextStyle(color: Colors.white, fontSize: 21.0),
                       ),
                       color: Colors.green,
                     ),
                   ),
                 ),
                 Text(
+                  _imcResult,
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(color: Colors.greenAccent[700], fontSize: 25.0),
+                ),
+                Text(
                   _infoText,
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  style:
+                      TextStyle(color: Colors.greenAccent[700], fontSize: 25.0),
                 ),
               ],
             ),
